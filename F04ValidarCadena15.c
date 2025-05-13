a478cf464007db65c6fe6b8f962c30555d0d627d
zuricatty <gzurisadai.des@gmail.com>
2025-05-12 19:34:51 -0600

    Add initial version of F04ValidarCadena15.c

---
+++ F04ValidarCadena15.c
@@ -0,0 +1,67 @@
+/* ******
+* Función que permita leer cadenas sólo de 15 caracteres cualesquiera (números o letras, no más ni menos de 15) y retorne la cadena leída sólo hasta que sea correcta.
+* Autor: Zuri
+****** */
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char* ValidarCadena15() {
+
+    static char sCadena[20];
+    int eLongitudCadena, eContador, eCadenaValida;
+
+    while (1) {
+
+        printf("Ingrese una cadena de 15 caracteres: ");
+        gets(sCadena);
+
+        eLongitudCadena = strlen(sCadena);
+
+        if (eLongitudCadena > 15) {
+
+            printf("Error. La cadena es muy larga.\n");
+
+        } else if (eLongitudCadena < 15) {
+
+            printf("Error. La cadena es muy corta.\n");
+
+        } else {
+
+            eCadenaValida = 1;
+            eContador = 0;
+
+            while(sCadena[eContador] != '\0') {
+
+                if (!isalnum(sCadena[eContador])) {
+
+                    eCadenaValida = 0;
+                }
+
+                eContador++;
+            }
+
+            if (!eCadenaValida) {
+
+                printf("Error. La cadena contiene caracteres no permitidos.\n");
+
+            } else {
+
+                return sCadena;
+            }
+        }
+    }
+}
+
+//Probar [Borrar]
+int main() {
+    char* cadenaValida;
+
+    cadenaValida = ValidarCadena15();
+
+    printf("Cadena válida ingresada: %s\n", cadenaValida);
+
+    getchar();
+    return 0;
+}
