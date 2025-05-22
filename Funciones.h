bb516cfa5856d6f52c80d768a8ff66298b1d729e
OscarGGSS <oscarggss@comunidad.unam.mx>
2025-05-21 21:54:24 -0600

    Update Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -490,3 +490,53 @@ void Conversion() {
 }
 
 
+/* *********
+   Programa: F19CadenaANumerico.c
+   Autor: Oscar
+   Fecha:
+   Objetivo:
+   Compilador: Dev-C++ 5.11
+   Version 1.0
+********* */
+
+
+
+// --- Prototipos ---
+char* leerCadena();
+double convertirCadenaANumero(const char*);
+void mostrarNumero(double);
+void ejecutarConversion();
+
+
+char* leerCadena() {
+    char* entrada = (char*)malloc(100 * sizeof(char)); 
+    if (entrada != NULL) {
+        printf("Ingresa un n%cmero (puede tener punto decimal): ", 163);
+        scanf("%99s", entrada); 
+    }
+    return entrada;
+}
+
+
+double convertirCadenaANumero(const char* cadena) {
+    return strtod(cadena, NULL); 
+}
+
+void mostrarNumero(double fNumero) {
+    printf("N%cmero convertido: %f\n", 163, fNumero);
+}
+
+void ejecutarConversion() {
+    char* cadena = leerCadena();
+    if (cadena != NULL) {
+        double numero = convertirCadenaANumero(cadena);
+        mostrarNumero(numero);
+        free(cadena); 
+    } else {
+        printf("Error al leer la cadena.\n");
+    }
+}
+
+
+
+
