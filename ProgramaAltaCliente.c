da4ddf4ceb9b79f4cffcddf822efa5bb17ee39b7
zuricatty <gzurisadai.des@gmail.com>
2025-05-21 23:27:52 -0600

    - intefaz.h

--- ProgramaAltaCliente.c
+++ ProgramaAltaCliente.c
@@ -14,7 +14,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <windows.h>
-#include "interfaz.h"
 #include "Funciones.h"
 
 //Constantes
@@ -25,6 +24,7 @@
 int main(){
 
     //Variables
+
 
     Marco();
     //Margen(2, 5, 10, 40); no se si asi se va allamar la funcion
